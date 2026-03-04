c63b3103b0224d92839135cb318fddefef60cc07
Donovan <donosaurusjk@gmail.com>
2026-03-04 08:45:41 -0500

    encryption finally works aahahahah

--- index.js
+++ index.js
@@ -1,3 +1,50 @@
+function encryptMessage () {
+    console.log("hawk")
+    let message = document.getElementById("messageBox").value;
+    let keyword = (document.getElementById("keywordBox").value).toLowerCase();
+    var encryptedMessage = "";
+    const keyLetters = [];
+
+    for (let keyLetter of keyword) {
+        keyLetters.push(keyLetter);
+    }
+
+    for (let i = 1; i < keyLetters.length; i++) {
+        const letter = keyLetters[i];
+        let compareLetInd = i-1;
+
+        while (compareLetInd >= 0 && keyLetters[compareLetInd].charCodeAt(0) > letter.charCodeAt(0)) {
+            keyLetters[compareLetInd + 1] = keyLetters[compareLetInd];
+            compareLetInd--;
+        }
+        keyLetters[compareLetInd + 1] = letter;
+    }
+
+    var previousInd = 0;
+    console.log(keyLetters)
+
+    for (let i = 0; i < keyLetters.length; i++) {
+        for (let j = 0; j < message.length; j = j + keyLetters.length) {
+            if ((keyword.indexOf(keyLetters[i], previousInd) + j) < message.length) {
+                encryptedMessage += message[keyword.indexOf(keyLetters[i], previousInd) + j]
+            }
+        }
+
+        if (keyLetters[i] == keyLetters[i+1%keyLetters.length]) {
+            previousInd = keyword.indexOf(keyLetters[i], previousInd) + 1;
+        }
+        else {previousInd = 0}
+        
+    }
+
+    outputBox(encryptedMessage);
+
+    //get index number of where the sorted letter is at
+
+    //encryptedMessage += message[]
+
+}
+
 function makeEncryptBox () {
     document.getElementById("word").innerHTML = '<label for="messageBox">Enter Message here --></label><input type="text" id="messageBox"></input>';
     document.getElementById("submit").innerHTML = '<input type="submit" value="Encrypt">';
@@ -10,28 +57,9 @@ function makeDecryptBox () {
     document.getElementById("submit").onclick = function() {decryptMessage();};
 }
 
-function encryptMessage () {
-    let message = document.getElementById("messageBox").value.toLowerCase();
-    let keyShift = Number(document.getElementById("keyShift").value)%26;
-    let output = "";
-    for (let i=0; i < message.length; i++) {
-        let char = message.charCodeAt(i);
-        if (char + keyShift > 122) {char -= 26;}
-        output += String.fromCharCode(char + keyShift);
-    }
-    outputBox(output);
-}
-
 function decryptMessage () {
-    let message = document.getElementById("messageBox").value.toLowerCase();
-    let keyShift = Number(document.getElementById("keyShift").value)%26;
-    let output = "";
-    for (let i=0; i < message.length; i++) {
-        let char = message.charCodeAt(i);
-        if (char - keyShift < 97) {char += 26;}
-        output += String.fromCharCode(char - keyShift);
-    }
-    outputBox(output);
+    
+    console.log("decrypting message");
 }
 
 function outputBox (output) {
