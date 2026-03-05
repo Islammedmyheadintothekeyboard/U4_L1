cba0e5c2ccc409186a22276325d5711d9e0693c0
Donovan <donosaurusjk@gmail.com>
2026-03-05 13:31:00 -0500

    encryption and decryption works

--- index.js
+++ index.js
@@ -1,32 +1,20 @@
 function encryptMessage () {
-    console.log("hawk")
     let message = document.getElementById("messageBox").value;
     let keyword = (document.getElementById("keywordBox").value).toLowerCase();
     var encryptedMessage = "";
-    const keyLetters = [];
-
-    for (let keyLetter of keyword) {
-        keyLetters.push(keyLetter);
-    }
-
-    for (let i = 1; i < keyLetters.length; i++) {
-        const letter = keyLetters[i];
-        let compareLetInd = i-1;
-
-        while (compareLetInd >= 0 && keyLetters[compareLetInd].charCodeAt(0) > letter.charCodeAt(0)) {
-            keyLetters[compareLetInd + 1] = keyLetters[compareLetInd];
-            compareLetInd--;
-        }
-        keyLetters[compareLetInd + 1] = letter;
-    }
+    const keyLetters = keyword.split("").sort();
 
     var previousInd = 0;
-    console.log(keyLetters)
 
     for (let i = 0; i < keyLetters.length; i++) {
         for (let j = 0; j < message.length; j = j + keyLetters.length) {
             if ((keyword.indexOf(keyLetters[i], previousInd) + j) < message.length) {
-                encryptedMessage += message[keyword.indexOf(keyLetters[i], previousInd) + j]
+                if (message[keyword.indexOf(keyLetters[i], previousInd) + j] === " ") {
+                    encryptedMessage += "_";
+                }
+                else {
+                    encryptedMessage += message[keyword.indexOf(keyLetters[i], previousInd) + j]
+                }
             }
         }
 
@@ -38,11 +26,55 @@ function encryptMessage () {
     }
 
     outputBox(encryptedMessage);
+}
 
-    //get index number of where the sorted letter is at
+function decryptMessage () {
+    let message = document.getElementById("messageBox").value;
+    let keyword = (document.getElementById("keywordBox").value).toLowerCase();
+    let decryptedMessage = "";
+    const message2D = [];
+    const keyLetters = keyword.split("").sort();
 
-    //encryptedMessage += message[]
+    for (let i = 0; i < Math.ceil(message.length / keyword.length); i++) {
+        message2D.push([]);
+        for (let j = 0; j < keyword.length; j++) {
+            message2D[i].push(null);
+        }
+    }
 
+    const hangOver = message.length % keyword.length;
+    let previousInd = 0;
+    let tracker = 0;
+    let indexOfKeyLetter;
+    let lengthOfSeg;
+
+    for (let i = 0; i < keyword.length; i++) {
+        indexOfKeyLetter = keyword.indexOf(keyLetters[i], previousInd);
+
+        if (indexOfKeyLetter < hangOver) {lengthOfSeg = Math.ceil(message.length / keyword.length);} 
+        else {lengthOfSeg = Math.floor(message.length / keyword.length);}
+
+        for (let j = 0; j < lengthOfSeg; j++) {
+            message2D[j][indexOfKeyLetter] = message[tracker];
+            tracker++;
+        }
+
+        //I don't know what this is for, yet I know what its for.
+        if (keyLetters[i] == keyLetters[i+1%keyLetters.length]) {
+            previousInd = keyword.indexOf(keyLetters[i], previousInd) + 1;
+        }
+        else {previousInd = 0}
+    }
+    
+    for (let i = 0; i < message2D.length; i++) {
+        for (let j = 0; j < message2D[i].length; j++) {
+            if (message2D[i][j] !== null) {
+                decryptedMessage += message2D[i][j];
+            }
+        }
+    }
+
+    outputBox(decryptedMessage);
 }
 
 function makeEncryptBox () {
@@ -55,11 +87,6 @@ function makeDecryptBox () {
     document.getElementById("word").innerHTML = '<label for="messageBox">Enter Encrypted Message here --></label><input type="text" id="messageBox"></input>';
     document.getElementById("submit").innerHTML = '<input type="submit" value="Decrypt">';
     document.getElementById("submit").onclick = function() {decryptMessage();};
-}
-
-function decryptMessage () {
-    
-    console.log("decrypting message");
 }
 
 function outputBox (output) {
